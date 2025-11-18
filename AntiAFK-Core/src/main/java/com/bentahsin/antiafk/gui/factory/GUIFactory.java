eb10c84ea783813b631cd5f07488a6c9f89d7e57
bentahsin <tahsin@bentahsin.com>
2025-11-19 01:21:19 +0300

    refactor: Simplify GUIFactory interface by removing unused imports and redundant line breaks

--- AntiAFK-Core/src/main/java/com/bentahsin/antiafk/gui/factory/GUIFactory.java
+++ AntiAFK-Core/src/main/java/com/bentahsin/antiafk/gui/factory/GUIFactory.java
@@ -1,26 +1,19 @@
 package com.bentahsin.antiafk.gui.factory;
 
-import com.bentahsin.antiafk.AntiAFKPlugin;
-import com.bentahsin.antiafk.gui.book.BookInputManager;
 import com.bentahsin.antiafk.gui.menus.*;
 import com.bentahsin.antiafk.gui.utility.PlayerMenuUtility;
-import com.google.inject.Provides;
-import com.google.inject.Singleton;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.Optional;
 import java.util.function.Consumer;
 
 /**
  * Guice'ın AssistedInject kullanarak GUI nesneleri oluşturmasını sağlayan fabrika arayüzü.
  */
 public interface GUIFactory {
-
     BehavioralAnalysisGUI createBehavioralAnalysisGUI(PlayerMenuUtility playerMenuUtility);
     SettingsGUI createSettingsGUI(PlayerMenuUtility playerMenuUtility);
     AdminPanelGUI createAdminPanelGUI(PlayerMenuUtility playerMenuUtility);
-
     ConfirmationGUI createConfirmationGUI(
             PlayerMenuUtility playerMenuUtility,
             String title,
@@ -28,18 +21,10 @@ public interface GUIFactory {
             Consumer<InventoryClickEvent> onConfirm,
             Consumer<InventoryClickEvent> onCancel
     );
-
-
     PlayerActionGUI createPlayerActionGUI(PlayerMenuUtility playerMenuUtility);
-
     PlayerListGUI createPlayerListGUI(PlayerMenuUtility playerMenuUtility, int page);
-
     RegionActionEditGUI createRegionActionEditGUI(PlayerMenuUtility playerMenuUtility);
-
     RegionActionsListGUI createRegionActionsListGUI(PlayerMenuUtility playerMenuUtility);
-
     RegionEditGUI createRegionEditGUI(PlayerMenuUtility playerMenuUtility);
-
-
     RegionListGUI createRegionListGUI(PlayerMenuUtility playerMenuUtility);
 }
